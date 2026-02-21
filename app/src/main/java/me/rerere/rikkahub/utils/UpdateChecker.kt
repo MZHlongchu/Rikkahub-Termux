9b30dff26e0b7085989f55a4fff851e49edee793
navy <2603451187@qq.com>
2026-02-21 22:30:36 +0800

    Update UpdateChecker.kt

--- app/src/main/java/me/rerere/rikkahub/utils/UpdateChecker.kt
+++ app/src/main/java/me/rerere/rikkahub/utils/UpdateChecker.kt
@@ -24,12 +24,12 @@ class UpdateChecker(private val client: OkHttpClient) {
 
     // DISABLED: Always returns a Flow that immediately emits an error state
     // Update checking is completely disabled to prevent network access
-    fun checkUpdate(): Flow<UiState<UpdateInfo>> = flow {
+    fun checkUpdate(): Flow<UiState<UpdateInfo>> = flow<UiState<UpdateInfo>> {
         emit(UiState.Loading)
         // Immediately fail - updates are disabled
         throw Exception("Update checking is disabled")
-    }.catch { e: Throwable ->  // 显式声明类型
-    emit(UiState.Error(e))
+    }.catch { e ->
+        emit(UiState.Error(e))
     }.flowOn(Dispatchers.IO)
 
     // DISABLED: Download functionality is also disabled
@@ -41,88 +41,13 @@ class UpdateChecker(private val client: OkHttpClient) {
 @Serializable
 data class UpdateDownload(
     val name: String,
-    val url: String,
-    val size: String
+    val url: String
 )
 
 @Serializable
 data class UpdateInfo(
-    val version: String,
-    val publishedAt: String,
+    val versionCode: Int,
+    val versionName: String,
     val changelog: String,
     val downloads: List<UpdateDownload>
 )
-
-/**
- * Version value class for semantic versioning comparison
- * Supports full SemVer: MAJOR.MINOR.PATCH[-prerelease][+build]
- */
-@JvmInline
-value class Version(val value: String) : Comparable<Version> {
-
-    private fun parse(): ParsedVersion {
-        val withoutBuild = value.split("+").first()
-        val hyphenIndex = withoutBuild.indexOf('-')
-        val (coreStr, prereleaseStr) = if (hyphenIndex >= 0) {
-            withoutBuild.substring(0, hyphenIndex) to withoutBuild.substring(hyphenIndex + 1)
-        } else {
-            withoutBuild to null
-        }
-        val core = coreStr.split(".").map { it.toIntOrNull() ?: 0 }
-        val prerelease = prereleaseStr?.split(".")
-        return ParsedVersion(core, prerelease)
-    }
-
-    override fun compareTo(other: Version): Int {
-        val a = this.parse()
-        val b = other.parse()
-
-        val maxLen = maxOf(a.core.size, b.core.size)
-        for (i in 0 until maxLen) {
-            val ap = if (i < a.core.size) a.core[i] else 0
-            val bp = if (i < b.core.size) b.core[i] else 0
-            if (ap != bp) return ap.compareTo(bp)
-        }
-
-        return when {
-            a.prerelease == null && b.prerelease == null -> 0
-            a.prerelease != null && b.prerelease == null -> -1
-            a.prerelease == null && b.prerelease != null -> 1
-            else -> comparePrerelease(a.prerelease!!, b.prerelease!!)
-        }
-    }
-
-    companion object {
-        fun compare(version1: String, version2: String): Int {
-            return Version(version1).compareTo(Version(version2))
-        }
-
-        private fun comparePrerelease(a: List<String>, b: List<String>): Int {
-            val maxLen = maxOf(a.size, b.size)
-            for (i in 0 until maxLen) {
-                if (i >= a.size) return -1
-                if (i >= b.size) return 1
-
-                val aNum = a[i].toIntOrNull()
-                val bNum = b[i].toIntOrNull()
-
-                val cmp = when {
-                    aNum != null && bNum != null -> aNum.compareTo(bNum)
-                    aNum != null -> -1
-                    bNum != null -> 1
-                    else -> a[i].compareTo(b[i])
-                }
-                if (cmp != 0) return cmp
-            }
-            return 0
-        }
-    }
-}
-
-private data class ParsedVersion(
-    val core: List<Int>,
-    val prerelease: List<String>?,
-)
-
-operator fun String.compareTo(other: Version): Int = Version(this).compareTo(other)
-operator fun Version.compareTo(other: String): Int = this.compareTo(Version(other))
