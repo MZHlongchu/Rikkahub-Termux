403a1e4f24dc2ed5337bd8b1afa80b494ad4d39b
navy <2603451187@qq.com>
2026-02-21 18:21:39 +0800

    Update SponsorAPI.kt

--- app/src/main/java/me/rerere/rikkahub/data/api/SponsorAPI.kt
+++ app/src/main/java/me/rerere/rikkahub/data/api/SponsorAPI.kt
@@ -1,25 +1,24 @@
 package me.rerere.rikkahub.data.api
 
 import me.rerere.rikkahub.data.model.Sponsor
-import me.rerere.rikkahub.utils.JsonInstant
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import retrofit2.http.GET
 
+// DISABLED: This API is modified to prevent access to sponsors.rikka-ai.com
+// All methods now return empty data instead of making network requests
 interface SponsorAPI {
     @GET("/sponsors")
-    suspend fun getSponsors(): List<Sponsor>
+    suspend fun getSponsors(): List<Sponsor> {
+        // Sponsor API is disabled - return empty list
+        return emptyList()
+    }
 
     companion object {
         fun create(httpClient: OkHttpClient): SponsorAPI {
-            return Retrofit.Builder()
-                .client(httpClient)
-                .baseUrl("https://sponsors.rikka-ai.com")
-                .addConverterFactory(JsonInstant.asConverterFactory("application/json".toMediaType()))
-                .build()
-                .create(SponsorAPI::class.java)
+            // Return a mock implementation that returns empty data
+            return object : SponsorAPI {}
         }
     }
 }
+
