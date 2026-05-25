11fed3087ebf2752d9bcd1d1a573b37af251d458
sybdz <207881135+sybdz@users.noreply.github.com>
2026-05-25 23:32:59 +0800

    Fix web UI logo formatting

--- web-ui/app/components/logo.tsx
+++ web-ui/app/components/logo.tsx
@@ -1,16 +1,5 @@
 import type { ComponentPropsWithoutRef } from "react";
 
-export default function Logo({
-  alt = "RikkaHub-Lune",
-  ...props
-}: ComponentPropsWithoutRef<"img">) {
-  return (
-    <img
-      alt={alt}
-      src="/app-icon.png"
-      width={256}
-      height={256}
-      {...props}
-    />
-  );
+export default function Logo({ alt = "RikkaHub-Lune", ...props }: ComponentPropsWithoutRef<"img">) {
+  return <img alt={alt} src="/app-icon.png" width={256} height={256} {...props} />;
 }
