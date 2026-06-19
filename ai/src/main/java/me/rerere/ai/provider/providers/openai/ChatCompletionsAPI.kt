6a1dfd5c516dce4aa4b50db772adc1c469e1c19e
re-ovo <1609403959@qq.com>
2026-06-19 15:40:56 +0800

    适配 aiping.cn 思考参数

--- ai/src/main/java/me/rerere/ai/provider/providers/openai/ChatCompletionsAPI.kt
+++ ai/src/main/java/me/rerere/ai/provider/providers/openai/ChatCompletionsAPI.kt
@@ -400,6 +400,10 @@ class ChatCompletionsAPI(
                         }
                     }
 
+                    "aiping.cn" -> {
+                        put("enable_thinking", level.isEnabled)
+                    }
+
                     "open.bigmodel.cn" -> {
                         put("thinking", buildJsonObject {
                             put("type", if (!level.isEnabled) "disabled" else "enabled")
